916df9d9134ea723f77b6629ad7e4b6e7e4f50fd
Andrea Birotti <andrea.birotti@gmail.com>
2015-02-27 22:04:33 +0000

    3% on to trees

---
+++ src/PF_3_Trees.scala
@@ -0,0 +1,31 @@
+sealed trait Tree[+A]
+case class Leaf[A](value: A) extends Tree[A]
+case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
+
+object Tree {
+
+  def size[A](tree: Tree[A]): Int = tree match {
+    case Leaf(_) => 1
+    case Branch(l, r) => size(l) + size(r)
+  }
+
+  def maximum(tree: Tree[Int]): Int = tree match {
+    case Leaf(x) => x
+    case Branch(l, r) => maximum(l) max maximum(r)
+  }
+
+  def depth[A](tree: Tree[A]): Int = tree match {
+    case Leaf(x) => 1
+    case Branch(l, r) => depth(l) + depth(r)
+  }
+
+  def map[A,B](tree: Tree[A])(f: A => B): Tree[B] = tree match {
+    case Leaf(x) => Leaf(f(x))
+    case Branch(l, r) => Branch(map(l)(f), map(r)(f))
+  }
+
+  def fold[A,B](tree: Tree[A])(id: A=>B)(f: (A, A)=>B): B = tree match{
+    case Leaf(x) => id(x)
+    case Branch(l, r) =>
+  }
+}
