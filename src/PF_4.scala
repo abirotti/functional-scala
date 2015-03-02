5823369c9423b79a689d37ca7aeab96fdd3f3a69
abirotti <andrea.birotti@gmail.com>
2015-03-02 23:51:05 +0000

    Options

--- src/PF_4.scala
+++ src/PF_4.scala
@@ -1,17 +1,28 @@
 sealed trait Option[+A] {
 
+  //TODO: make flatMap, orElse, filter typecheck without matching
+
   def map[B](f: A => B): Option[B] = this match {
     case None => None
     case Some(a) => Some(f(a))
   }
 
-  def flatMap[B](f: A => Option[B]): Option[B] = ???
+  def flatMap[B](f: A => Option[B]): Option[B] = this match {
+    case Some(x) => f(x)
+    case None => None
+  }
   def getOrElse[B >: A](default: => B): B = this match {
     case None => default
     case Some(a) => a
   }
-  def orElse[B >: A](ob: => Option[B]): Option[B] = ???
-  def filter(f: A => Boolean): Option[A] = ???
+  def orElse[B >: A](ob: => Option[B]): Option[B] = ob match {
+    case Some(_) => ob
+    case _ => this
+  }
+  def filter(f: A => Boolean): Option[A] = this match {
+    case Some(x) if (f(x)) => this
+    case _ => None
+  }
 }
 case class Some[+A](get:A) extends Option[A]
 case object None extends Option[Nothing]
