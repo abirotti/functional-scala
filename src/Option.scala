569d1044748541c550951737480e905bb08d1588
abirotti <andrea.birotti@gmail.com>
2015-03-05 21:29:32 +0000

    stuck on sequence

--- src/Option.scala
+++ src/Option.scala
@@ -6,7 +6,6 @@ sealed trait Option[+A] {
     case None => None
     case Some(a) => Some(f(a))
   }
-
   def flatMap[B](f: A => Option[B]): Option[B] = this match {
     case Some(x) => f(x)
     case None => None
@@ -15,18 +14,30 @@ sealed trait Option[+A] {
     case None => default
     case Some(a) => a
   }
-  def orElse[B >: A](ob: => Option[B]): Option[B] = ob match {
-    case Some(_) => ob
-    case _ => this
+  def orElse[B >: A](ob: => Option[B]): Option[B] = this match {
+    case Some(_) => this
+    case _ => ob
   }
   def filter(f: A => Boolean): Option[A] = this match {
-    case Some(x) if (f(x)) => this
+    case Some(x) if f(x) => this
     case _ => None
   }
 }
+
 case class Some[+A](get:A) extends Option[A]
 case object None extends Option[Nothing]
 
-object PF_4 {
+object Option {
+  def mean(xs: Seq[Double]): Option[Double] =
+    if (xs.isEmpty) None
+    else Some(xs.sum / xs.length)
+
+  def variance(xs: Seq[Double]): Option[Double] =
+    mean(xs) flatMap(x => mean(xs map(y => math.pow( y - x, 2 ))))
+
+  def map2[A,B,C](a: Option[A], b: Option[B])(f: (A,B) => C): Option[C] =
+    a flatMap(a2 => b map(b2 => f(a2, b2)))
+
+  def sequence[A](a: List[Option[A]]): Option[List[A]] = ???
 
 }
