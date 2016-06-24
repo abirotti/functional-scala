bf9d4a45956b12ff5167a5711e7d77c85926dfef
Andrea Birotti <andrea.birotti@gmail.com>
2016-06-24 11:56:40 +0100

    [options] some more tests implemented

--- src/test/scala-2.11/com/abirotti/functionalScala/OptionsTest.scala
+++ src/test/scala-2.11/com/abirotti/functionalScala/OptionsTest.scala
@@ -20,8 +20,30 @@ class OptionsTest extends FunSuite with ShouldMatchers{
     Some(1).getOrElse(2) should be (1)
   }
 
-  test("getOr"){1 should be(2)}
-  test("filter"){1 should be(2)}
-  test("flatMap"){1 should be(2)}
+  test("None.orElse should return the other option"){
+    None.orElse(Some(1)) should be(Some(1))
+  }
+
+  test("Some(1).orElse should return the other option"){
+    Some(1).orElse(Some(2)) should be(Some(1))
+  }
+
+  test("filter on None should be None"){
+    None.filter(_ == 2) should be(None)
+  }
+
+  test("filter"){
+    Some(1).filter(_ == 2) should be(None)
+    Some(1).filter(_ == 1) should be(Some(1))
+  }
+
+  test("flatMap"){
+    None.flatMap(someIfEven) should be(None)
+    Some(1).flatMap(someIfEven) should be(None)
+    Some(2).flatMap(someIfEven) should be(Some(2))
+  }
+
+  def someIfEven: (Int) => Optionz[Int] = v => if (v % 2 == 0) Some(v) else None
+
   test("variance"){1 should be(2)}
 }
