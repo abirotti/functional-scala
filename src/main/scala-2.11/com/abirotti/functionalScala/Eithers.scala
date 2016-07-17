0350837c2be79a6543d0801b32d0b17d94be50ac
abirotti <andrea.birotti@gmail.com>
2016-07-17 19:41:13 +0100

    [Either] sequence and traverse

--- src/main/scala-2.11/com/abirotti/functionalScala/Eithers.scala
+++ src/main/scala-2.11/com/abirotti/functionalScala/Eithers.scala
@@ -20,8 +20,17 @@ object Eithers {
       case Left(a) => Left(a)
     }
 
-  def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C) =
-    this flatMap( tt => b map (bb => f(tt, bb)) )
+    def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C) =
+      this flatMap (tt => b map (bb => f(tt, bb)))
+
+    def sequence[E, A](es: List[Either[E, A]]): Either[E, List[A]] = es match {
+      case Nil => Right(Nil)
+      case e :: es => e flatMap (ee => sequence(es) map (ee :: _))
+    }
+    def traverse[E, A, B](es: List[A])(f: A => Either[E, B]): Either[E, List[B]] = es match {
+      case Nil => Right(Nil)
+      case e :: es => f(e) flatMap (ee => traverse(es)(f) map (ee :: _))
+    }
   }
 
   case class Left[+E](value: E) extends Either[E, Nothing]
