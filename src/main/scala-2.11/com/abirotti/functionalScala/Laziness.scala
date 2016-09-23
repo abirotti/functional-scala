a5da79f7d927c62a762bb14a8b9d48772bbec606
andrea.birotti <andrea.birotti@gmail.com>
2016-09-23 17:25:04 +0100

    some more work on Streams

--- src/main/scala-2.11/com/abirotti/functionalScala/Laziness.scala
+++ src/main/scala-2.11/com/abirotti/functionalScala/Laziness.scala
@@ -8,9 +8,39 @@ sealed trait Stream[+A] {
     case _ => List()
   }
 
-  def take(i: Int): Stream[A] = ???
+  def take(i: Int): Stream[A] = this match {
+    case Cons(h, t) if i > 1 => cons(h(), t().take(i - 1))
+    case Cons(h, t) if i == 1 => cons(h(), empty)
+    case _ => empty
+  }
 
-  def drop(i: Int): Stream[A] = ???
+  def drop(i: Int): Stream[A] = this match {
+    case Cons(_, t) if i > 0 => t().drop(i - 1)
+    case _ => this
+  }
+
+  def takeWhile(p: A => Boolean): Stream[A] = this match {
+    case Cons(h, t) if p(h()) => cons(h(), t().takeWhile(p))
+    case _ => empty
+  }
+
+  def takeWhile2(p: A => Boolean): Stream[A] =
+    foldRight(empty[A])((h,t) => if(p(h)) cons(h,t) else empty
+    )
+
+  def exists(p: A => Boolean): Boolean =
+    foldRight(false)((a, b) => p(a) || b)
+
+  def foldRight[B](z: => B)(f: (A, => B) => B): B =
+    this match {
+      case Cons(h,t) => f(h(), t().foldRight(z)(f))
+      case _ => z
+    }
+
+  def forAll(p: A => Boolean): Boolean = this match {
+    case Cons(h,t) => if(p(h())) t().forAll(p) else false
+    case Empty => true
+  }
 }
 case object Empty extends Stream[Nothing]
 case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A]
--- src/test/scala-2.11/com/abirotti/functionalScala/LazinessTest.scala
+++ src/test/scala-2.11/com/abirotti/functionalScala/LazinessTest.scala
@@ -1,14 +1,14 @@
-package com.abirotti.fpinscala.laziness
+package com.abirotti.functionalScala
 
-import org.scalatest.{ShouldMatchers, FunSuite}
+import org.scalatest.{FunSuite, ShouldMatchers}
 
-class StreamsTests extends FunSuite with ShouldMatchers{
+class StreamsTests extends FunSuite with ShouldMatchers {
 
-  val nineNumbers = Stream(1 to 10)
+  val nineNumbers = Stream(1,2,3,4,5,6,7,8,9)
   val emptyStream = Stream()
 
   test("toList should return a list of the stream") {
-    nineNumbers.toList should be(List(1 to 10))
+    nineNumbers.toList should be(List(1,2,3,4,5,6,7,8,9))
   }
 
   test("toList should return Nil on empty stream") {
@@ -16,10 +16,42 @@ class StreamsTests extends FunSuite with ShouldMatchers{
   }
 
   test("drop(n) drops the first n elements of the stream"){
-    nineNumbers.drop(3) should be(List(4 to 10))
+    nineNumbers.drop(3).toList should be(List(4,5,6,7,8,9))
+  }
+
+  test("drop(0) returns the stream"){
+    nineNumbers.drop(0).toList should be(List(1,2,3,4,5,6,7,8,9))
   }
 
   test("take(n) takes the first n elements of the stream"){
-    nineNumbers.take(3) should be(List(1 to 5))
+    nineNumbers.take(3).toList should be(List(1,2,3))
+  }
+
+  test("take(0) returns the empty stream"){
+    nineNumbers.take(0).toList should be(Nil)
+  }
+
+  test("nineNumbers.takeWhile(_<4) returns Stream(1,2,3)") {
+    nineNumbers.takeWhile(_<4).toList should be(List(1,2,3))
+  }
+
+  test("nineNumbers.takeWhile(_>40) returns empty Stream") {
+    nineNumbers.takeWhile(_>40).toList should be(Nil)
+  }
+
+  test("nineNumbers.takeWhile2(_<4) returns Stream(1,2,3)") {
+    nineNumbers.takeWhile2(_<4).toList should be(List(1,2,3))
+  }
+
+  test("nineNumbers.takeWhile2(_>40) returns empty Stream") {
+    nineNumbers.takeWhile2(_>40).toList should be(Nil)
+  }
+
+  test("nineNumbers.forAll(_%2==0) return false"){
+    nineNumbers.forAll(_%2==0) should be(false)
+  }
+
+  test("nineNumbers.forAll(_<20) return true"){
+    nineNumbers.forAll(_<20) should be(true)
   }
 }
