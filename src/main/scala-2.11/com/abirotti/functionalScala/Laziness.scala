77228a6a53adab5cf744aa3eebc64e540bdfa811
abirotti <andrea.birotti@gmail.com>
2016-11-02 12:07:12 +0000

    [Laziness] drop and take (and remove the pom)

--- src/main/scala-2.11/com/abirotti/functionalScala/Laziness.scala
+++ src/main/scala-2.11/com/abirotti/functionalScala/Laziness.scala
@@ -1,29 +1,38 @@
 package com.abirotti.functionalScala
 
-import Stream._
-sealed trait Stream[+A] {
+import com.abirotti.functionalScala.MyStream.cons
+
+sealed trait MyStream[+A] {
 
   def toList: List[A] = this match {
     case Cons(h, t) =>  h() :: t().toList
     case _ => List()
   }
 
-  def take(i: Int): Stream[A] = ???
+  def take(i: Int): MyStream[A] = this match {
+    case Empty => Empty
+    case Cons(h, t) if i == 0 => Empty
+    case Cons(h, t) if i > 0 => cons(h(), t().take(i-1))
+  }
 
-  def drop(i: Int): Stream[A] = ???
+  def drop(i: Int): MyStream[A] = this match {
+    case Empty => Empty
+    case Cons(h, t) if i > 0 => t().drop(i-1)
+    case x => x
+  }
 }
-case object Empty extends Stream[Nothing]
-case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A]
+case object Empty extends MyStream[Nothing]
+case class Cons[+A](h: () => A, t: () => MyStream[A]) extends MyStream[A]
 
-object Stream {
-  def cons[A](hd: => A, tl: => Stream[A]): Stream[A] = {
+object MyStream {
+  def cons[A](hd: => A, tl: => MyStream[A]): MyStream[A] = {
     lazy val head = hd
     lazy val tail = tl
     Cons(() => head, () => tail)
   }
 
-  def empty[A]: Stream[A] = Empty
+  def empty[A]: MyStream[A] = Empty
 
-  def apply[A](as: A*): Stream[A] =
+  def apply[A](as: A*): MyStream[A] =
     if (as.isEmpty) empty else cons(as.head, apply(as.tail: _*) )
 }
--- src/test/scala-2.11/com/abirotti/functionalScala/LazinessTest.scala
+++ src/test/scala-2.11/com/abirotti/functionalScala/LazinessTest.scala
@@ -1,25 +1,26 @@
 package com.abirotti.fpinscala.laziness
 
-import org.scalatest.{ShouldMatchers, FunSuite}
+import com.abirotti.functionalScala.MyStream
+import org.scalatest.{FunSuite, ShouldMatchers}
 
-class StreamsTests extends FunSuite with ShouldMatchers{
+class LazinessTest extends FunSuite with ShouldMatchers{
 
-  val nineNumbers = Stream(1 to 10)
-  val emptyStream = Stream()
+  val tenNumbers = MyStream(1 to 10:_*)
+  val emptyStream = MyStream()
 
   test("toList should return a list of the stream") {
-    nineNumbers.toList should be(List(1 to 10))
+    tenNumbers.toList should be((1 to 10).toList)
   }
 
   test("toList should return Nil on empty stream") {
     emptyStream.toList should be(Nil)
   }
 
-  test("drop(n) drops the first n elements of the stream"){
-    nineNumbers.drop(3) should be(List(4 to 10))
+  test("drop(n) drops the first n elements of the stream") {
+    tenNumbers.drop(3).toList should be((4 to 10).toList)
   }
 
-  test("take(n) takes the first n elements of the stream"){
-    nineNumbers.take(3) should be(List(1 to 5))
+  test("take(n) takes the first n elements of the stream") {
+    tenNumbers.take(3).toList should be((1 to 3).toList)
   }
 }
